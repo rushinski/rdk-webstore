9a210c34c6f826e4e35c87b29dff05551c5bc2d8
Jacob Rushinski <dsrush2@verizon.net>
2025-12-30 00:41:04 -0500

    brand image caching error fixed + supabase.co whitelisted in next.config.js so that we can pull images from our supabase bucket

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseHostname = supabaseUrl ? new URL(supabaseUrl).hostname : null;
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      ...(supabaseHostname
+        ? [
+            {
+              protocol: "https",
+              hostname: supabaseHostname,
+              pathname: "/storage/v1/object/public/**",
+            },
+          ]
+        : []),
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
