81fb50e3cc438ec9756245c24b25611b4fe5bad4
Jacob Rushinski <dsrush2@verizon.net>
2026-01-24 10:25:30 -0500

    staging github actions workflow change + small minor tweaks

--- src/config/constants/contact.ts
+++ src/config/constants/contact.ts
@@ -1,3 +1,3 @@
 // src/config/constants/contact.ts
-export const SUPPORT_EMAIL = "dsrush13@gmail.com";
+export const SUPPORT_EMAIL = "realdealholyspill@gmail.com";
 export const BUG_REPORT_EMAIL = "jrushinski@sneakereco.com";
